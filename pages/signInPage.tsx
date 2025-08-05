35ee069b8153daf718919cbdacdd3818f823a265
luongcaochanhuyit <161787746+LuongCaoChanHuyIT@users.noreply.github.com>
2025-08-05 16:49:36 +0700

    final

--- pages/signInPage.tsx
+++ pages/signInPage.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function SignInPage() {
   return (
-    <View>
-      <Text>signInPage sfasfdasdfasdf</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500 text-2xl font-bold">Hello Tailwind + Expo!</Text>
     </View>
   )
 }
